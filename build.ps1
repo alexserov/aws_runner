7fac030ff70a6ba18e1e91b1d8776535d694dc3c
Alexey Serov <alexey.serov@devexpress.com>
2021-10-04 10:19:25 +0300

    fix powershell login

--- build.ps1
+++ build.ps1
@@ -1,7 +1,13 @@
 Push-Location .\docker
 
+Write-Host "Building images"
 docker compose build
-(Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin $env:AWS_ACCOUNT_ID.dkr.ecr.$env:AWS_REGION.amazonaws.com
+
+Write-Host "AWS login"
+$hostaddress = $env:AWS_ACCOUNT_ID+".dkr.ecr."+$env:AWS_REGION+".amazonaws.com"
+(Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin $hostaddress
+
+Write-Host "Pushing images"
 docker compose push
 
 Pop-Location
